3c4fd7b1f2f5593ed26eedd386c72a0aa815f290
Nhat Duy <duy950630@gmail.com>
2023-11-15 22:50:29 +0700

    Update chức năng capcha

--- src/views/Login.js
+++ src/views/Login.js
@@ -2,18 +2,33 @@ import "./Login.css"
 import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Login = () =>{
 
     const navigate = useNavigate();
-    
+    const [isCaptchaVerified, setCaptchaVerified] = useState(false);
+
     const handleRegister = () => {
         navigate('/register');
     };
 
-    const handleHome = () =>{
-        navigate('/');
+    const handleHome = () => {
+        if (isCaptchaVerified) {
+            // Perform login action
+            navigate('/');
+        } else {
+            // Display an error or prompt to complete reCAPTCHA
+            console.error("Please complete reCAPTCHA");
+        }
     }
+
+    const handleCaptchaChange = (value) => {
+        // Handle reCAPTCHA verification status
+        setCaptchaVerified(true);
+    }
+
     return(
         <>
             <h2 className="animate-charcter1">LOGIN</h2> 
@@ -28,6 +43,10 @@ const Login = () =>{
                     <label for="password">Mật khẩu</label>
                     <input type="password" id="password" name="password" required/>
                 </div>
+                <ReCAPTCHA className="capcha"
+                        sitekey="6LcHTw8pAAAAAMuaJGh85LoqP4tTbNGz2pB2vfY0"
+                        onChange={handleCaptchaChange}
+                />
                 <div className="button-group    ">
                     <button type="submit" onClick={handleHome}>Login</button>
                     <button type="button" onClick={handleRegister}>Register</button>
